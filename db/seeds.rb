a91668fd57e82273c459fa65262a3525d08b989c
Pauline <paulinegouttiere@gmail.com>
2023-01-04 23:57:57 +0100

    seed livres echec

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,98 +14,98 @@ User.destroy_all
 
 puts 'creating user'
 
-user1 = User.create(
+user1 = User.create!(
   email: "user1@default.com",
   password: "1234567"
 )
 
 puts 'creating books'
 
-livre1 = Livre.create(
+livre1 = Livre.create!(
   titre: "Le mage du Kremlin",
   auteur: "Giuliano da Empoli",
   note: 3
 )
 
-livre2 = Livre.create(
+livre2 = Livre.create!(
   titre: "Les enfants sont rois",
   auteur: "Delphine de Vigan",
   note: 3
 )
 
-livre3 = Livre.create(
+livre3 = Livre.create!(
   titre: "Le poids du papillon",
   auteur: "Erri de Luca",
   note: 2
 )
 
-livre4 = Livre.create(
+livre4 = Livre.create!(
   titre: "GPS",
   auteur: "Lucie Rico",
   note: 1
 )
 
-livre5 = Livre.create(
+livre5 = Livre.create!(
   titre: "L’invention de nos vies",
   auteur: "Karine Tuil",
   note: 3
 )
 
-livre6 = Livre.create(
+livre6 = Livre.create!(
   titre: "La place",
   auteur: "Annie Ernaux",
   note: 3
 )
 
-livre7 = Livre.create(
+livre7 = Livre.create!(
   titre: "La treizième heure",
   auteur: "Emmanuelle Bayamack Tam",
   note: 2
 )
 
-livre8 = Livre.create(
+livre8 = Livre.create!(
   titre: "Madame Sagan",
   auteur: "Geneviève Moll",
   note: 2
 )
 
-livre9 = Livre.create(
+livre9 = Livre.create!(
   titre: "Lady Chevy",
   auteur: "John Woods",
   note: 2
 )
 
-livre10 = Livre.create(
+livre10 = Livre.create!(
   titre: "Le doorman",
   auteur: "Madeleine Assas",
   note: 4
 )
 
-livre11 = Livre.create(
+livre11 = Livre.create!(
   titre: "Avant que la nuit ne m’emporte",
   auteur: "Guy Cuevas",
   note: 3
 )
 
-livre12 = Livre.create(
+livre12 = Livre.create!(
   titre: "Lumière d’été puis vient la nuit",
   auteur: "Jon Kalman Stefansson",
   note: 2
 )
 
-livre13 = Livre.create(
+livre13 = Livre.create!(
   titre: "La panthère des neiges",
   auteur: "Sylvain Tesson",
   note: 2
 )
 
-livre14 = Livre.create(
+livre14 = Livre.create!(
   titre: "L’affaire Alaska Sanders",
   auteur: "Joël Dicker",
   note: 2
 )
 
-livre15 = Livre.create(
+livre15 = Livre.create!(
   titre: "L’histoire du fils",
   auteur: "Marie Hélène Lafon",
   note: 3
