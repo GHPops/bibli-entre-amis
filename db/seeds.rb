e33425a44f3792a624a3e3e5d885c49dee0b8cb6
Pauline <paulinegouttiere@gmail.com>
2023-01-05 01:17:39 +0100

    seed livres

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,106 +14,131 @@ User.destroy_all
 
 puts 'creating user'
 
-user1 = User.create(
+user1 = User.create!(
   email: "user1@default.com",
   password: "1234567"
 )
 
-user2 = User.create(
+user2 = User.create!(
   email: "user2@default.com",
+  password: "1234567"
+)
+
+user3 = User.create!(
+  email: "user3@default.com",
+  password: "1234567"
+)
+
+user4 = User.create!(
+  email: "user4@default.com",
   password: "1234567"
 )
 
 puts 'creating books'
 
-livre1 = Livre.create(
+livre1 = Livre.create!(
   titre: "Le mage du Kremlin",
   auteur: "Giuliano da Empoli",
-  note: 3
+  note: 3,
+  user_id: "user1"
 )
 
-livre2 = Livre.create(
+livre2 = Livre.create!(
   titre: "Les enfants sont rois",
   auteur: "Delphine de Vigan",
-  note: 3
+  note: 3,
+  user_id: "user2"
 )
 
-livre3 = Livre.create(
+livre3 = Livre.create!(
   titre: "Le poids du papillon",
   auteur: "Erri de Luca",
-  note: 2
+  note: 2,
+  user_id: "user3"
 )
 
-livre4 = Livre.create(
+livre4 = Livre.create!(
   titre: "GPS",
   auteur: "Lucie Rico",
-  note: 1
+  note: 1,
+  user_id: "user4"
 )
 
-livre5 = Livre.create(
+livre5 = Livre.create!(
   titre: "L’invention de nos vies",
   auteur: "Karine Tuil",
-  note: 3
+  note: 3,
+  user_id: "user1"
 )
 
-livre6 = Livre.create(
+livre6 = Livre.create!(
   titre: "La place",
   auteur: "Annie Ernaux",
-  note: 3
+  note: 3,
+  user_id: "user2"
 )
 
-livre7 = Livre.create(
+livre7 = Livre.create!(
   titre: "La treizième heure",
   auteur: "Emmanuelle Bayamack Tam",
-  note: 2
+  note: 2,
+  user_id: "user3"
 )
 
-livre8 = Livre.create(
+livre8 = Livre.create!(
   titre: "Madame Sagan",
   auteur: "Geneviève Moll",
-  note: 2
+  note: 2,
+  user_id: "user4"
 )
 
-livre9 = Livre.create(
+livre9 = Livre.create!(
   titre: "Lady Chevy",
   auteur: "John Woods",
-  note: 2
+  note: 2,
+  user_id: "user1"
 )
 
-livre10 = Livre.create(
+livre10 = Livre.create!(
   titre: "Le doorman",
   auteur: "Madeleine Assas",
-  note: 4
+  note: 4,
+  user_id: "user2"
 )
 
-livre11 = Livre.create(
+livre11 = Livre.create!(
   titre: "Avant que la nuit ne m’emporte",
   auteur: "Guy Cuevas",
-  note: 3
+  note: 3,
+  user_id: "user3"
 )
 
-livre12 = Livre.create(
+livre12 = Livre.create!(
   titre: "Lumière d’été puis vient la nuit",
   auteur: "Jon Kalman Stefansson",
-  note: 2
+  note: 2,
+  user_id: "user4"
 )
 
-livre13 = Livre.create(
+livre13 = Livre.create!(
   titre: "La panthère des neiges",
   auteur: "Sylvain Tesson",
-  note: 2
+  note: 2,
+  user_id: "user1"
 )
 
-livre14 = Livre.create(
+livre14 = Livre.create!(
   titre: "L’affaire Alaska Sanders",
   auteur: "Joël Dicker",
-  note: 2
+  note: 2,
+  user_id: "user2"
 )
 
-livre15 = Livre.create(
+livre15 = Livre.create!(
   titre: "L’histoire du fils",
   auteur: "Marie Hélène Lafon",
-  note: 3
+  note: 3,
+  user_id: "user3"
 )
 
 puts "finished"
