ab8ade5bdb1e71c125673fac3ed126ffe3183dbf
Pauline <paulinegouttiere@gmail.com>
2023-01-05 18:25:55 +0100

    index et show livres

--- app/controllers/livres_controller.rb
+++ app/controllers/livres_controller.rb
@@ -1,5 +1,5 @@
 class LivresController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  # skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_livre, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -14,6 +14,13 @@ class LivresController < ApplicationController
   end
 
   def create
+    @livre = Livre.new(livre_params)
+    if @livre.save
+      redirect_to livres_path, notice: "Livre ajouté à la base!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+    authorize @livre
   end
 
   def edit
@@ -28,7 +35,7 @@ class LivresController < ApplicationController
   private
 
   def set_livre
-    @livre = Livre.find(params[:livre_id])
+    @livre = Livre.find(params[:id])
   end
 
   def livre_params
