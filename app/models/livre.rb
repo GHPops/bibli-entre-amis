079df4a0776dcca2ddf9b0deabc551e43648be45
Pauline <paulinegouttiere@gmail.com>
2023-02-24 23:32:50 +0100

    bold bouton ajouter

--- app/models/livre.rb
+++ app/models/livre.rb
@@ -5,4 +5,8 @@ class Livre < ApplicationRecord
   validates :titre, presence: true
   validates :auteur, presence: true
   validates :note, numericality: { only_integer: true }, length: { maximum: 5 }
+
+  scope :past, -> { where("fin < ?", Date.today) }
+  scope :upcoming, -> { where("debut > ?", Date.today) }
+  scope :current, -> { where("debut <= ? AND fin >= ?", Date.today, Date.today) }
 end
